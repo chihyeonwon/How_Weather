c480276eb49b954705ab6df019df37e88a00805b
ylose <heungeob1003@gmail.com>
2024-06-09 15:01:39 +0900

    Add Weather List Fragment

--- app/src/main/java/com/example/howweather/WeatherListFragment.kt
+++ app/src/main/java/com/example/howweather/WeatherListFragment.kt
@@ -1,44 +1,49 @@
 package com.example.howweather
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.howweather.databinding.FragmentWeatherListBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class WeatherListFragment : Fragment() {
-
     private var _binding: FragmentWeatherListBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
-
+    private val listAdapter by lazy { WeatherItemListAdapter() }
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentWeatherListBinding.inflate(inflater, container, false)
-        return binding.root
-
+        val root: View = binding.root
+        return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        val dataList = WeatherDataList.list
+        with(binding) {
+            weatherList.apply {
+                adapter = listAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL).apply {
+                    getDrawable(requireContext(), R.drawable.list_divider)?.let { setDrawable(it) }
+                }.also {
+                    addItemDecoration(it)
+                }
+                listAdapter.submitList(dataList.toList())
+            }
+            // 임시로 현재 날씨는 0번 Dummy Data로 설정
+            val currentWeather = dataList.get(0)
+            weatherStatusIv.setImageResource(currentWeather.skyStatus.colorIcon)
+            mainWeatherText.text = currentWeather.skyStatus.text
+            mainTemperTv.text = currentWeather.temperature
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
