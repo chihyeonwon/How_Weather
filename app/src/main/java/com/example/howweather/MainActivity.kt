bc482175c32b171f0e053cadd6a80e8c266b5507
ylose <heungeob1003@gmail.com>
2024-06-10 19:37:55 +0900

    두 번째 Fragment로 이동하는 코드 삭제

--- app/src/main/java/com/example/howweather/MainActivity.kt
+++ app/src/main/java/com/example/howweather/MainActivity.kt
@@ -16,7 +16,6 @@ class MainActivity : AppCompatActivity() {
             viewPager.adapter = adapter
             indicator.setViewPager(viewPager)
             adapter.registerAdapterDataObserver(indicator.adapterDataObserver)
-            viewPager.currentItem = 1 // Weather 리스트 페이지(1번 페이지)가 제일 먼저 오도록
         }
     }
 }
