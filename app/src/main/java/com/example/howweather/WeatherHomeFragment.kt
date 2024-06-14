3b21352d8b1eaa688a97fe9449f196695dd462a9
ylose <heungeob1003@gmail.com>
2024-06-14 14:16:51 +0900

    viewModel을 이용한 데이터 업데이트

--- app/src/main/java/com/example/howweather/WeatherHomeFragment.kt
+++ app/src/main/java/com/example/howweather/WeatherHomeFragment.kt
@@ -1,49 +1,51 @@
 package com.example.howweather
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
+import com.example.howweather.data.cityList
 import com.example.howweather.databinding.FragmentWeatherHomeBinding
-import com.example.howweather.repository.WeatherRepository
+import com.example.howweather.databinding.PopupListBinding
 import com.example.howweather.viewmodel.WeatherViewModel
-import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class WeatherHomeFragment : Fragment() {
-
     private var _binding: FragmentWeatherHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
-
-    private val currentDate by lazy {
-        SimpleDateFormat("yyyyMMdd", Locale.KOREA).format(Date())
-    }
-
+    private val currentDate by lazy { SimpleDateFormat("yyyyMMdd", Locale.KOREA).format(Date()) }
     private val viewModel by activityViewModels<WeatherViewModel>()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentWeatherHomeBinding.inflate(inflater, container, false)
-        with(binding) {
-            val repository = WeatherRepository()
-            lifecycleScope.launch {
-                val model = repository.getWeather(currentDate, "서울특별시")
-                val data = model.toWeatherData()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.selectRegionButton.setOnClickListener {
+            showCitiesPopup(it)
+        }
+
+        binding.selectedRegionText.setOnClickListener {
+            showCitiesPopup(it)
+        }
+
+        viewModel.weatherData.observe(viewLifecycleOwner) { data ->
+            with(binding) {
                 mainWeatherText.text = data.skyStatus.text
                 mainTemperTv.text = data.temperature
                 mainRainTv.text = data.rainState.value.toString()
@@ -54,17 +56,32 @@ class WeatherHomeFragment : Fragment() {
                 weatherStatusIv.setImageResource(data.skyStatus.colorIcon)
             }
         }
-        return binding.root
-
+        fetchWeatherData()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun showCitiesPopup(anchorView: View) {
+        val cities = cityList.map { it.name }.toList()
 
+        val popupBinding = PopupListBinding.inflate(layoutInflater, null, false).also {
+            it.cityList.adapter =
+                ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, cities)
+        }
+
+        PopupWindow(popupBinding.root, 500, LinearLayout.LayoutParams.WRAP_CONTENT, true).apply {
+            setBackgroundDrawable(getDrawable(requireContext(), R.drawable.rectangle_background))
+            elevation = 10f
+            showAsDropDown(anchorView)
+            popupBinding.cityList.setOnItemClickListener { _, _, position, _ ->
+                val selectedCity = cities[position]
+                binding.selectedRegionText.text = selectedCity
+                viewModel.setRegion(selectedCity)
+                fetchWeatherData(selectedCity)
+                dismiss()
+            }
+        }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun fetchWeatherData(city: String = "서울특별시") {
+        viewModel.getWeather(currentDate, city)
     }
 }
